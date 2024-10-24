4bbd8736452091b99c40e777d28e735f32560642
yuutakun-2 <arkarchanmyae2@gmail.com>
2024-10-24 21:53:04 +0630

    React Component Styles

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,5 +1,18 @@
+const styles = {
+  toolbar: {
+    background: "lightgray",
+    padding: 10,
+  },
+  darkHeader: {
+    color: "white",
+  },
+};
+
 export default function Toolbar(props) {
   return (
-    <div style={{ background: "lightgray", padding: 10 }}>{props.children}</div>
+    // Double curly braces in inline CSS because children should be object literal in React, not a string like in HTML
+    // Outer curly brace is to say that this is an object
+    // Inner curly brace is to say that this is JSX
+    <div style={styles.toolbar}>{props.children}</div> // No need double curly braces because styles.toolbar is already an object
   );
 }
