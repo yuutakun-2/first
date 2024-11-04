fef671ba4feee63a3e8bfc8ccc8cf3a6d5c943b5
yuutakun-2 <arkarchanmyae2@gmail.com>
2024-11-04 14:40:56 +0630

    API with react

--- src/App.js
+++ src/App.js
@@ -1,108 +1,35 @@
-import React, { useRef, useState } from "react";
-import { createContext, useContext } from "react";
-import Toolbar from "./Toolbar";
-function Item(props) {
-  return (
-    <li>
-      {props.name}, ${props.price}
-    </li>
-  );
-}
-
-function AddForm(props) {
-  const nameRef = useRef();
-  const priceRef = useRef();
-
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-
-        props.add(nameRef.current.value, priceRef.current.value);
-      }}
-    >
-      <input type="text" ref={nameRef} /> <br />
-      <input type="text" ref={priceRef} /> <br />
-      <button type="submit">Add</button>
-    </form>
-  );
-}
-
-// const MyContext = createContext();
-const ThemeContext = createContext();
-
+import { useState, useEffect } from "react";
 export default function App() {
-  const [theme, setTheme] = useState("light");
-  const [data, setData] = useState([
-    { id: 1, name: "Apple", price: 0.99 },
-    { id: 2, name: "Orange", price: 0.89 },
-  ]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      });
+  }, []);
 
-  const add = (name, price) => {
-    const id = data.length + 1;
-    setData([...data, { id, name, price }]);
-  };
+  function add() {
+    fetch("https://reqres.in/api/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ first_name: "Tom" }),
+    })
+      .then((res) => res.json())
+      .then((tom) => {
+        setUsers([...users, tom]);
+      });
+  }
   return (
     <div>
-      <h1>Hello React</h1>
       <ul>
-        {data.map((i) => (
-          <Item key={i.id} name={i.name} price={i.price} /> //key is needed in Virtual DOM
+        {users.map((u) => (
+          <li key={u.id}>{u.first_name}</li>
         ))}
       </ul>
-      <AddForm add={add} />
-      <Toolbar>
-        <h1>This is toolbar heading</h1>
-        <p>This is a paragraph</p>
-      </Toolbar>
-
-      {/* <MyContext.Provider value="Hello Context">
-        <Header />
-      </MyContext.Provider> */}
-
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        <div
-          style={{
-            minHeight: 400,
-            color: "green",
-            padding: 20,
-            background: theme === "light" ? "lightblue" : "darkblue",
-          }}
-        >
-          <Header />
-        </div>
-      </ThemeContext.Provider>
+      <button onClick={add}>New User</button>
     </div>
   );
 }
-
-function Header() {
-  return <Title />;
-  //   // const value = useContext(MyContext);
-  //   // return <h1>{value}</h1>;
-}
-
-function Title() {
-  // const value = useContext(MyContext);
-  const { theme, setTheme } = useContext(ThemeContext);
-  return (
-    <div>
-      <h1>Hello Context</h1>
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Toggle Theme
-      </button>
-    </div>
-  );
-}
-
-function reducer(state = [], action) {
-  if (action.type === "ADD") {
-    return [...state, action.name];
-  }
-  return state;
-}
-
-const store = Redux.createStore(reducer);
-store.dispatch({ type: "ADD", name: "Apple" });
-store.dispatch({ type: "ADD", name: "Orange" });
-store.dispatch({ type: "ADD", name: "Banana" });
